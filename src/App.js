9183a7d59cb90c4f58d9abad3a5f493092f74a17
RikeshThapa <rthapa@u.rochester.edu>
2020-08-26 16:31:25 -0500

    updating logo so we have a non generic React logo. Adding setNextQuestion function to update the state such that next question shows

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import Question from './components/Question';
 import quizQuestions from './api/quizQuestions';
+import Quiz from './components/Quiz';
+
+import logo from './xipi-logo.svg';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -17,7 +20,9 @@ class App extends Component {
       answersCount: {},
       result: ''
     };
-  }
+
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  };
 
   componentDidMount() {
     //ShuffleArray randomizes answer optionss
@@ -27,7 +32,7 @@ class App extends Component {
       question: quizQuestions[0].question,
       answerOptions: shuffledAnswerOptions[0]
     });
-  }
+  };
 
   shuffleArray(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -48,13 +53,53 @@ class App extends Component {
     return array;
   };
 
+
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+    if (this.state.questionId < quizQuestions.length) {
+        setTimeout(() => this.setNextQuestion(), 300);
+      } else {
+        // do nothing for now
+      }
+  };
+
+  setUserAnswer(answer) {
+    this.setState((state) => ({
+      answersCount: {
+        ...state.answersCount,
+        [answer]: (state.answersCount[answer] || 0) + 1
+      },
+      answer: answer
+    }));
+  }
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: ''
+    });
+  }
+
   render(){
     return (
       <div className="App">
         <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>React Quiz</h2>
         </div>
-        <Question content="What is your favourite food?" />
+        <Quiz
+          answer={this.state.answer}
+          answerOptions={this.state.answerOptions}
+          questionId={this.state.questionId}
+          question={this.state.question}
+          questionTotal={quizQuestions.length}
+          onAnswerSelected={this.handleAnswerSelected}
+        />
       </div>
     );
   }
