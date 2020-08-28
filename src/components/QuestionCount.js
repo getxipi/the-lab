456c339cccf63e612168bb92bdb903814d14f86a
RikeshThapa <rthapa@u.rochester.edu>
2020-08-28 11:17:06 -0500

    Adding a progress bar to return visual element of progress

--- src/components/QuestionCount.js
+++ src/components/QuestionCount.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Progress } from 'antd';
+
 
 function QuestionCount(props) {
   return (
     <div className="questionCount">
+      <Progress percent={(props.counter/props.total)*100} showInfo={false} />
       Question <span>{props.counter}</span> of <span>{props.total}</span>
     </div>
   );
