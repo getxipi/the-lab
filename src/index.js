56462b3292f3ebae37a5c5703c909c8e4aab40b0
RikeshThapa <rthapa@u.rochester.edu>
2020-08-28 10:48:27 -0500

    adding Antd components as a test, cleaning up index.js removing unnecessary component imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+//external packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +19,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+//render(<App />, document.getElementById('root'));
